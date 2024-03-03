6d009fa162666c47695f2aa3592378c515e30103
abhaymadanpatil31 <flutterabhay@gmail.com>
2024-03-03 16:43:14 +0530

    flashapp1

---
+++ flash1/flashapp1/lib/flashApp1.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class flashApp1 extends StatefulWidget {
+  const flashApp1({super.key});
+
+  @override
+  State createState() => _flashApp1State();
+}
+
+class _flashApp1State extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("flash1"),
+        leading: Row(
+          children: [
+            GestureDetector(child: Icon(Icons.person)),
+          ],
+        ),
+        actions: [
+          GestureDetector(
+            child: const Icon(Icons.perm_camera_mic),
+          ),
+          GestureDetector(
+            child: const Icon(Icons.join_left_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+}
